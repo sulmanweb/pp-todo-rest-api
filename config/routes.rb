06ccb538fc7d525074b04b2e2a93b4b7c5675b41
Sulman Baig <sulmanweb@gmail.com>
2019-10-06 11:46:38 +0500

    Adding swagger docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "/api-docs/v1" => redirect("/dist/index.html?url=/apidocs/v1.json")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
