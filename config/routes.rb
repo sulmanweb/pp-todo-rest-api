3ca5cbea81a5e5d61dc7a4fbe171ad999fd65396
Sulman Baig <sulmanweb@gmail.com>
2019-10-06 11:52:00 +0500

    Adding global errors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match "/404", to: "error/errors#not_found", via: :all
+  match "/500", to: "error/errors#internal_server_error", via: :all
   get "/api-docs/v1" => redirect("/dist/index.html?url=/apidocs/v1.json")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
